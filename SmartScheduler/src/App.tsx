2d3a3abe339f07b6caf88b888069751e4e675197
mbuck29 <micheal.buckendahl@icloud.com>
2025-10-21 17:37:16 -0500

    Added MUI library as well as provided an exmaple with a button

--- SmartScheduler/src/App.tsx
+++ SmartScheduler/src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import HelloWorld from "./components/helloWorld";
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <>
       <div>SmartScheduler</div>
       <HelloWorld />
+      <Button variant="contained" onClick={() => console.log("hello")}>
+        Contained
+      </Button>
     </>
   );
 }
