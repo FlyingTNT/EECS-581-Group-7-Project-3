2b998d4fe8e02c693f9d5227a7c8663467c237cb
mbuck29 <micheal.buckendahl@icloud.com>
2025-10-21 14:37:26 -0500

    created a hello world example

--- SmartScheduler/src/App.tsx
+++ SmartScheduler/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import HelloWorld from "./components/helloWorld";
 
 function App() {
-  return <div>SmartScheduler</div>;
+  return (
+    <>
+      <div>SmartScheduler</div>
+      <HelloWorld />
+    </>
+  );
 }
 
 export default App;
