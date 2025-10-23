85ad2ed466ed1f91ae9399bb634af6fd713beadb
mbuck29 <micheal.buckendahl@icloud.com>
2025-10-23 12:28:15 -0500

    Added Redux to the repo as well as an example with some dummy course data

---
+++ SmartScheduler/src/features/scheduleSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { Course } from "../types";
+
+type ScheduleState = {
+  sections: Course[];
+};
+
+const initialState: ScheduleState = {
+  sections: [],
+};
+
+const scheduleSlice = createSlice({
+  name: "schedule",
+  initialState,
+  reducers: {
+    addSection(state, action: PayloadAction<Course>) {
+      state.sections.push(action.payload);
+    },
+    removeSection(state, action: PayloadAction<string>) {
+      state.sections = state.sections.filter((s) => s.id !== action.payload);
+    },
+    clearSchedule(state) {
+      state.sections = [];
+    },
+  },
+});
+
+export const { addSection, removeSection, clearSchedule } =
+  scheduleSlice.actions;
+export default scheduleSlice.reducer;
---
+++ SmartScheduler/src/types.ts
@@ -0,0 +1,8 @@
+export interface Course {
+  id: string;
+  name: string;
+  creditHours: number;
+  instructor: string;
+  times: string[];
+  description: string;
+}
