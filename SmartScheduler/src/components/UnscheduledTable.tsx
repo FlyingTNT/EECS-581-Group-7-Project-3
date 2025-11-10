ceabe29aeba047fb779bfabac4a91d2f25525998
C. Cooper <c.cooper@ku.edu>
2025-11-10 12:08:09 -0600

    Remove css conflict
    .dropDownButton in UnscheduledTableStyles.css was overriding .dropDownButton in ClassCardStyles.css

--- SmartScheduler/src/components/UnscheduledTable.tsx
+++ SmartScheduler/src/components/UnscheduledTable.tsx
@@ -31,7 +31,7 @@ export default function UnscheduledTable() {
         </div>
 
         <div 
-          className="dropDownButton">
+          className="unsDropDownButton">
           {isDropDownOpen ? "/\\" : "\\/"}
         </div>
       </div>
