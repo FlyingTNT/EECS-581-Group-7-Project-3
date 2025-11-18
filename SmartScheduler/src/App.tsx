bc4b9f6d0dddc5850c0ef5e78fa7e96b89ac0a9f
C. Cooper <c.cooper@ku.edu>
2025-11-18 17:12:03 -0600

    Allow rotating permutations with arrow keys

--- SmartScheduler/src/App.tsx
+++ SmartScheduler/src/App.tsx
@@ -15,12 +15,41 @@ import TotalCreditHours from "./components/TotalCreditHours";
 import Grid from "./components/Grid";
 import UnscheduledTable from "./components/UnscheduledTable";
 import PaginationControls from "./components/PaginationControls";
+import { getState } from "./utils/Utilities";
+import { useAppDispatch } from "./redux/hooks";
+import { decrementCurrentPermutation, incrementCurrentPermutation } from "./features/scheduleSlice";
+import type { KeyboardEvent } from "react";
 
 function App() {
+  const state = getState();
+  const dispatch = useAppDispatch();
+
+  const onKeyUp = async (event: KeyboardEvent) =>
+  {
+    if(!(event.key === "ArrowLeft" || event.key === "ArrowRight"))
+    {
+      return;
+    }
+
+    if(state.currentPermutation === -1 || state.permutations.length === 0)
+    {
+      return;
+    }
+
+    if(event.key === "ArrowLeft")
+    {
+      dispatch(incrementCurrentPermutation());
+    }
+    else
+    {
+      dispatch(decrementCurrentPermutation());
+    }
+  }
+
   return (
     <>
       <Header />
-      <div className="contentContainer">
+      <div className="contentContainer" tabIndex={-1} onKeyUp={onKeyUp}>
         <div className="searchAndSelected">
           <div className="searchBarContainer">
             <SearchBar />
